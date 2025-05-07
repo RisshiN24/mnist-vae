e23dd40a3183a619d29112d86748ec41c68d1ca8
RisshiN24 <r.naavaal1@gmail.com>
2025-05-07 16:30:16 -0400

    First commit

---
+++ CONTRIBUTING.md
@@ -0,0 +1,14 @@
+## Commit Message Guidelines
+
+Use this structure: `<type>: <short summary>`
+
+### Allowed Types
+
+- **feat**: New feature
+- **fix**: Bug fix
+- **docs**: Documentation only
+- **style**: Formatting, no logic change
+- **refactor**: Refactoring code
+- **test**: Adding or updating tests
+- **chore**: Maintenance tasks
+- **exp**: Experimenting with feature
---
+++ src/mnist_vae.py
@@ -0,0 +1,144 @@
+# Import libraries
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras import layers # type: ignore
+from tensorflow.keras.callbacks import EarlyStopping # type: ignore
+import matplotlib.pyplot as plt
+
+# Encoder class
+class Encoder(tf.keras.Model):
+  def __init__(self, latent_dim):
+    super(Encoder, self).__init__()
+    self.dense1 = layers.Dense(512, activation='relu')
+    self.dense2 = layers.Dense(256, activation='relu')
+    self.z_mean = layers.Dense(latent_dim)
+    self.z_log_var = layers.Dense(latent_dim)
+
+  def call(self, x):
+    h1 = self.dense1(x)
+    h2 = self.dense2(h1)
+    z_mean = self.z_mean(h2)
+    z_log_var = self.z_log_var(h2)
+
+    # Reparameterization trick
+    eps = tf.random.normal(shape=tf.shape(z_mean)) # Noise
+    z = z_mean + tf.exp(0.5 * z_log_var) * eps
+    return z, z_mean, z_log_var
+
+# Decoder class
+class Decoder(tf.keras.Model):
+  def __init__(self, original_dim):
+    super().__init__()
+    self.dense1 = layers.Dense(256, activation='relu')
+    self.dense2 = layers.Dense(512, activation='relu')
+    self.output_layer = layers.Dense(original_dim, activation='sigmoid')
+
+  def call(self, z):
+    h1 = self.dense1(z)
+    h2 = self.dense2(h1)
+    reconstructed = self.output_layer(h2)
+    return reconstructed
+
+# VAE (Variational Autoencoder)
+class VAE(tf.keras.Model):
+    def __init__(self, encoder, decoder, beta=1.0):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.beta = beta  # Weight for KL divergence
+
+    def call(self, x):
+        z, z_mean, z_log_var = self.encoder(x)
+        reconstructed = self.decoder(z)
+        return reconstructed
+
+    # Custom train step because we have custom loss
+    def train_step(self, data):
+        if isinstance(data, tuple):
+            data = data[0]  # ignore labels
+
+        with tf.GradientTape() as tape:
+            z, z_mean, z_log_var = self.encoder(data)
+            reconstruction = self.decoder(z)
+
+            # Reconstruction loss (BCE)
+            reconstruction_loss = tf.reduce_mean(tf.square(data - reconstruction))
+
+
+            # KL divergence loss
+            kl_loss = -0.5 * tf.reduce_sum(1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var), axis=1)
+
+            total_loss = tf.reduce_mean(reconstruction_loss + self.beta * kl_loss)
+
+        # Backpropagation
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+
+        return {"loss": total_loss} # We'll track during training
+    
+# Load data
+def load_data():
+    (x_train, _), _ = tf.keras.datasets.mnist.load_data()
+    x_train = x_train.astype("float32") / 255.0 # Rescale
+    x_train = x_train.reshape(-1, 28*28)
+
+    # Return data
+    return x_train
+
+x_train = load_data()
+
+# Train model
+def train_model(latent_dim=2):
+    original_dim = 784  # 28x28 images, flattened
+
+    # Initialize parts
+    encoder = Encoder(latent_dim)
+    decoder = Decoder(original_dim)
+    vae = VAE(encoder, decoder, beta=1.0) # Can play around with
+
+    # Add an learning rate scheduler
+    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+        initial_learning_rate=0.0005,
+        decay_steps=1000,
+        decay_rate=0.95
+    )
+
+    # Compile with Adam optimizer
+    vae.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lr_schedule))
+
+    # Train
+    vae.fit(x_train, epochs=20, batch_size=128)
+
+    # Return model
+    return vae
+
+vae = train_model()
+
+###----------------------------------------------------###
+
+# Visualize results
+def visualize_results(vae, x_train):
+    num_samples = 10
+    x_sample = x_train[:num_samples]
+
+    # Reconstruct
+    x_reconstructed = vae.predict(x_sample)
+
+    # Plot
+    plt.figure(figsize=(20,4))
+    for i in range(num_samples):
+        ax = plt.subplot(2, num_samples, i + 1)
+        plt.imshow(x_sample[i].reshape(28, 28), cmap='gray')
+        plt.title("Original")
+        plt.axis('off')
+
+        ax = plt.subplot(2, num_samples, i + 1 + num_samples)
+        plt.imshow(x_reconstructed[i].reshape(28, 28), cmap='gray')
+        plt.title("Reconstructed")
+        plt.axis('off')
+
+    plt.tight_layout()
+    plt.show()
+
+visualize_results(vae, x_train)
