48f7260a05d932bf9e5e52a965972f8e1af5878e
RisshiN24 <r.naavaal1@gmail.com>
2025-05-09 10:51:46 -0400

    feat: Transition to Conv-VAE architecture

--- src/mnist_vae.py
+++ src/mnist_vae.py
@@ -7,38 +7,48 @@ from tensorflow.keras.callbacks import EarlyStopping # type: ignore
 import matplotlib.pyplot as plt
 
 # Encoder class
-class Encoder(tf.keras.Model):
-  def __init__(self, latent_dim):
-    super(Encoder, self).__init__()
-    self.dense1 = layers.Dense(512, activation='relu')
-    self.dense2 = layers.Dense(256, activation='relu')
-    self.z_mean = layers.Dense(latent_dim)
-    self.z_log_var = layers.Dense(latent_dim)
+class ConvEncoder(tf.keras.Model):
+    def __init__(self, latent_dim):
+        super(ConvEncoder, self).__init__()
+        self.conv1 = layers.Conv2D(32, kernel_size=3, strides=2, padding='same', activation='relu')  # (28,28,1) -> (14,14,32)
+        self.conv2 = layers.Conv2D(64, kernel_size=3, strides=2, padding='same', activation='relu')  # (14,14,32) -> (7,7,64)
+        self.flatten = layers.Flatten()  # (7,7,64) -> (3136,)
+        self.dense = layers.Dense(128, activation='relu')  # Optional intermediate layer
+        self.z_mean = layers.Dense(latent_dim)  # (128,) -> (latent_dim,)
+        self.z_log_var = layers.Dense(latent_dim)
 
-  def call(self, x):
-    h1 = self.dense1(x)
-    h2 = self.dense2(h1)
-    z_mean = self.z_mean(h2)
-    z_log_var = self.z_log_var(h2)
+    def call(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.flatten(x)
+        x = self.dense(x)
+        z_mean = self.z_mean(x)
+        z_log_var = self.z_log_var(x)
 
-    # Reparameterization trick
-    eps = tf.random.normal(shape=tf.shape(z_mean)) # Noise
-    z = z_mean + tf.exp(0.5 * z_log_var) * eps
-    return z, z_mean, z_log_var
+        # Reparameterization trick
+        eps = tf.random.normal(shape=tf.shape(z_mean))
+        z = z_mean + tf.exp(0.5 * z_log_var) * eps
+        return z, z_mean, z_log_var
 
 # Decoder class
-class Decoder(tf.keras.Model):
-  def __init__(self, original_dim):
-    super().__init__()
-    self.dense1 = layers.Dense(256, activation='relu')
-    self.dense2 = layers.Dense(512, activation='relu')
-    self.output_layer = layers.Dense(original_dim, activation='sigmoid')
+class ConvDecoder(tf.keras.Model):
+    def __init__(self):
+        super(ConvDecoder, self).__init__()
+        self.dense = layers.Dense(7 * 7 * 64, activation='relu')  # (latent_dim,) -> (3136,)
+        self.reshape = layers.Reshape((7, 7, 64))  # (3136,) -> (7, 7, 64)
 
-  def call(self, z):
-    h1 = self.dense1(z)
-    h2 = self.dense2(h1)
-    reconstructed = self.output_layer(h2)
-    return reconstructed
+        self.deconv1 = layers.Conv2DTranspose(64, kernel_size=3, strides=2, padding='same', activation='relu')  # (7,7,64) -> (14,14,64)
+        self.deconv2 = layers.Conv2DTranspose(32, kernel_size=3, strides=2, padding='same', activation='relu')  # (14,14,64) -> (28,28,32)
+        self.output_layer = layers.Conv2DTranspose(1, kernel_size=3, padding='same', activation='sigmoid')  # (28,28,32) -> (28,28,1)
+
+    def call(self, z):
+        x = self.dense(z)
+        x = self.reshape(x)
+        x = self.deconv1(x)
+        x = self.deconv2(x)
+        reconstructed = self.output_layer(x)
+        return reconstructed
+
 
 # VAE (Variational Autoencoder)
 class VAE(tf.keras.Model):
@@ -63,7 +73,7 @@ class VAE(tf.keras.Model):
             reconstruction = self.decoder(z)
 
             # Reconstruction loss (BCE)
-            reconstruction_loss = tf.keras.losses.binary_crossentropy(data, reconstruction)
+            reconstruction_loss = tf.reduce_sum(tf.keras.losses.binary_crossentropy(data, reconstruction), axis=[1, 2]) # BCE calculation reduces to (batch_size, 28, 28); use axis=1,2 for reduce_sum(...)
 
             # KL divergence loss
             kl_loss = -0.5 * tf.reduce_sum(1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var), axis=1)
@@ -80,7 +90,7 @@ class VAE(tf.keras.Model):
 def load_data():
     (x_train, _), _ = tf.keras.datasets.mnist.load_data()
     x_train = x_train.astype("float32") / 255.0 # Rescale
-    x_train = x_train.reshape(-1, 28*28)
+    x_train = x_train.reshape(-1, 28, 28, 1)
 
     # Return data
     return x_train
@@ -89,11 +99,10 @@ x_train = load_data()
 
 # Train model
 def train_model(latent_dim=10):
-    original_dim = 784  # 28x28 images, flattened
 
     # Initialize parts
-    encoder = Encoder(latent_dim)
-    decoder = Decoder(original_dim)
+    encoder = ConvEncoder(latent_dim)
+    decoder = ConvDecoder()
     vae = VAE(encoder, decoder, beta=1.0) # Can play around with
 
     # Add an learning rate scheduler
@@ -107,7 +116,7 @@ def train_model(latent_dim=10):
     vae.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lr_schedule))
 
     # Train
-    vae.fit(x_train, epochs=20, batch_size=128)
+    vae.fit(x_train, epochs=3, batch_size=128)
 
     # Return model
     return vae
